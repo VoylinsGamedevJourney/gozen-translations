e9188c5c944825b064090975a70dfc4c9e5e89ea
voylin <0voylin0@gmail.com>
2024-04-03 15:19:58 +0900

    Switching to Python

---
+++ generate_mo_files.py
@@ -0,0 +1,23 @@
+import os
+
+
+def generate_mo_files():
+    print('-= Generating *.mo files =-')
+    current_dir = os.path.dirname(__file__)
+    po_files_dir = os.path.join(current_dir, 'po_files')
+
+    os.chdir(po_files_dir) # Change directory to po_files
+
+    for po_file in os.listdir('.'):
+        if po_file.endswith('.po'):
+            mo_file = os.path.join(current_dir, '../src/translations/', po_file[:-3] + '.mo')
+            os.system(f'msgfmt -o {mo_file} {po_file}')
+
+            print(f'Generated: {po_file[:-3]}.mo')
+
+    os.chdir(current_dir) # Go back to the original directory
+    print('-= Generating localization files complete! =-')
+
+
+if __name__ == '__main__'
+    generate_mo_files()
